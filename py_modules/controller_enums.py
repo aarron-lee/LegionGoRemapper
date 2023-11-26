1da5a138ec8a981efb3620f683b1f5db1cad4866
Aarron Lee <aclee91@gmail.com>
2023-11-25 23:05:13 -0500

    add remappable button enums + setter methods

--- py_modules/controller_enums.py
+++ py_modules/controller_enums.py
@@ -4,7 +4,43 @@ class Controller(Enum):
     LEFT = 0x03
     RIGHT = 0x04
 
-# CONTROLLER = {
-#     "LEFT": 0x03,
-#     "RIGHT": 0x04
-# }
+# The Button to Remap
+class RemappableButtons(Enum):
+    Y1 = 0x1c
+    Y2 = 0x1d
+    Y3 = 0x1e
+    M2 = 0x21
+    M3 = 0x22
+
+# The action to assign to Remappable Buttons
+class RemapActions(Enum):
+    DISABLED = 0x00
+    L_STICK_CLICK = 0x03
+    L_STICK_UP = 0x04
+    L_STICK_DOWN = 0x05
+    L_STICK_LEFT = 0x06
+    L_STICK_RIGHT = 0x07
+
+    R_STICK_CLICK = 0x08
+    R_STICK_UP = 0x09
+    R_STICK_DOWN = 0x0a
+    R_STICK_LEFT = 0x0b
+    R_STICK_RIGHT = 0x0c
+
+    D_PAD_UP = 0x0d
+    D_PAD_DOWN = 0x0e
+    D_PAD_LEFT = 0x0f
+    D_PAD_RIGHT = 0x10
+
+    BUTTON_A = 0x12
+    BUTTON_B = 0x13
+    BUTTON_X = 0x14
+    BUTTON_Y = 0x15
+
+    L_BUMPER = 0x16
+    L_TRIGGER = 0x17
+    R_BUMPER = 0x18
+    R_TRIGGER = 0x19
+
+    VIEW = 0x23
+    MENU = 0x24
--- src/backend/utils.tsx
+++ src/backend/utils.tsx
@@ -1,28 +1,39 @@
 import { ServerAPI } from 'decky-frontend-lib';
-import { ControllerType } from './constants';
+import { ControllerType, RemapActions, RemappableButtons } from './constants';
 
 export enum ServerAPIMethods {
   RGB_ON = 'rgb_on',
-  RGB_OFF = 'rgb_off'
+  RGB_OFF = 'rgb_off',
+  REMAP_BUTTON = 'remap_button'
 }
 
-export const createRgbOn =
+const createRgbOn =
   (serverAPI: ServerAPI) => async (controller: ControllerType) => {
     await serverAPI.callPluginMethod(ServerAPIMethods.RGB_ON, {
       controller
     });
   };
 
-export const createRgbOff =
+const createRgbOff =
   (serverAPI: ServerAPI) => async (controller: ControllerType) => {
     await serverAPI.callPluginMethod(ServerAPIMethods.RGB_OFF, {
       controller
     });
   };
 
+const createRemapButtons =
+  (serverAPI: ServerAPI) =>
+  async (button: RemappableButtons, action: RemapActions) => {
+    await serverAPI.callPluginMethod(ServerAPIMethods.REMAP_BUTTON, {
+      button,
+      action
+    });
+  };
+
 export const createServerApiHelpers = (serverAPI: ServerAPI) => {
   return {
     rgbOn: createRgbOn(serverAPI),
-    rgbOff: createRgbOff(serverAPI)
+    rgbOff: createRgbOff(serverAPI),
+    remapButton: createRemapButtons(serverAPI)
   };
 };
