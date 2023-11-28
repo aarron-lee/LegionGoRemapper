539de6177554ec07f556f3294d3f70804804eba7
Aarron Lee <aclee91@gmail.com>
2023-11-28 16:56:26 -0500

    refactor rgb settings into reusable component

--- src/components/rgb/ControllerLightingPanel.tsx
+++ src/components/rgb/ControllerLightingPanel.tsx
@@ -1,18 +1,9 @@
-import {
-  ToggleField,
-  SliderField,
-  PanelSection,
-  ServerAPI,
-  gamepadSliderClasses,
-  ButtonItem
-} from 'decky-frontend-lib';
+import { ToggleField, PanelSection, ServerAPI } from 'decky-frontend-lib';
 import { VFC } from 'react';
 import { useState } from 'react';
-import { useRgb, useRgbProfileDisplayName, useRgbMode } from '../../hooks/rgb';
-import RgbModeSlider from './RgbModeSlider';
-import { RgbModes } from '../../backend/constants';
-import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import { useRgbProfileDisplayName } from '../../hooks/rgb';
 import { RgbPerGameProfilesToggle } from './RgbPerGameProfilesToggle';
+import { RgbSettings } from './RgbSettings';
 
 const DEFAULT_STATE = {
   isTouchpad: true
@@ -21,30 +12,8 @@ const DEFAULT_STATE = {
 const ControllerLightingPanel: VFC<{ serverAPI: ServerAPI }> = ({
   serverAPI
 }) => {
-  const [leftMode] = useRgbMode('LEFT');
-  const [rightMode] = useRgbMode('RIGHT');
-
   const displayName = useRgbProfileDisplayName();
   const [isTouchpad, setIsTouchpad] = useState(DEFAULT_STATE.isTouchpad);
-
-  const [showRightOptions, setShowRightOptions] = useState(false);
-  const [showLeftOptions, setShowLeftOptions] = useState(false);
-
-  const leftRgb = useRgb('LEFT');
-  const {
-    enabled: isLeftRgbOn,
-    brightness: brightnessL,
-    speed: speedL,
-    hue: hueL
-  } = leftRgb.rgbInfo;
-
-  const rightRgb = useRgb('RIGHT');
-  const {
-    enabled: isRightRgbOn,
-    brightness: brightnessR,
-    speed: speedR,
-    hue: hueR
-  } = rightRgb.rgbInfo;
 
   const TPadToggleChange = (value: boolean) => {
     setIsTouchpad(value);
@@ -61,171 +30,8 @@ const ControllerLightingPanel: VFC<{ serverAPI: ServerAPI }> = ({
     <PanelSection title={title}>
       <div>
         <RgbPerGameProfilesToggle />
-        <ToggleField
-          label="Right Controller LED"
-          checked={isRightRgbOn}
-          onChange={rightRgb.setEnabled}
-          bottomSeparator={isRightRgbOn ? 'none' : 'thick'}
-        />
-        {isRightRgbOn && (
-          <>
-            <ButtonItem
-              layout={'below'}
-              bottomSeparator={showRightOptions ? 'none' : 'thick'}
-              style={{
-                width: '100%',
-                height: '20px',
-                display: 'flex', // Set the display to flex
-                justifyContent: 'center', // Center align horizontally
-                alignItems: 'center' // Center align vertically
-              }}
-              onClick={() => setShowRightOptions(!showRightOptions)}
-            >
-              {showRightOptions ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-            </ButtonItem>
-          </>
-        )}
-        {showRightOptions && isRightRgbOn && (
-          <>
-            <RgbModeSlider controller="RIGHT" />
-            <SliderField
-              label="R-Stick Brightness"
-              valueSuffix="%"
-              value={brightnessR}
-              showValue={true}
-              min={1}
-              max={100}
-              validValues="range"
-              bottomSeparator={'none'}
-              onChange={(value) => rightRgb.updateBrightness(value)}
-            ></SliderField>
-            {rightMode !== RgbModes.SOLID && (
-              <SliderField
-                label="Speed"
-                valueSuffix="%"
-                value={speedR}
-                showValue={true}
-                min={1}
-                max={100}
-                validValues="range"
-                onChange={(value) => rightRgb.setSpeed(value)}
-              ></SliderField>
-            )}
-            {rightMode !== RgbModes.DYNAMIC && (
-              <>
-                <div className="ColorPicker_HSlider">
-                  <SliderField
-                    showValue
-                    label="Hue"
-                    value={hueR}
-                    min={0}
-                    max={359}
-                    validValues="range"
-                    bottomSeparator="thick"
-                    onChange={(value) => rightRgb.setHue(value)}
-                  />
-                </div>
-              </>
-            )}
-          </>
-        )}
-
-        <ToggleField
-          label="Left Controller LED"
-          checked={isLeftRgbOn}
-          bottomSeparator={isLeftRgbOn ? 'none' : 'thick'}
-          onChange={leftRgb.setEnabled}
-        />
-        {isLeftRgbOn && (
-          <>
-            <ButtonItem
-              layout={'below'}
-              bottomSeparator={'thick'}
-              style={{
-                width: '100%',
-                height: '20px',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center'
-              }}
-              onClick={() => setShowLeftOptions(!showLeftOptions)}
-            >
-              {showLeftOptions ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
-            </ButtonItem>
-          </>
-        )}
-        {showLeftOptions && isLeftRgbOn && (
-          <>
-            <RgbModeSlider controller="LEFT" />
-            <SliderField
-              label="L-Stick Brightness"
-              valueSuffix="%"
-              value={brightnessL}
-              showValue={true}
-              min={1}
-              max={100}
-              validValues="range"
-              bottomSeparator={'none'}
-              onChange={(value) => leftRgb.updateBrightness(value)}
-            ></SliderField>
-            {leftMode !== RgbModes.SOLID && (
-              <SliderField
-                label="Speed"
-                valueSuffix="%"
-                value={speedL}
-                showValue={true}
-                min={1}
-                max={100}
-                validValues="range"
-                onChange={(value) => leftRgb.setSpeed(value)}
-              ></SliderField>
-            )}
-            {leftMode !== RgbModes.DYNAMIC && (
-              <>
-                <div className="ColorPicker_HSlider">
-                  <SliderField
-                    showValue
-                    label="Hue"
-                    value={hueL}
-                    min={0}
-                    max={359}
-                    validValues="range"
-                    bottomSeparator="thick"
-                    onChange={(value) => leftRgb.setHue(value)}
-                  />
-                </div>
-                {/* <div
-                  style={{
-                    width: '100%',
-                    height: '30px',
-                    backgroundColor: `rgb(${redL}, ${greenL}, ${blueL})`,
-                    border: '1px solid black',
-                    borderRadius: '10px'
-                  }}
-                /> */}
-              </>
-            )}
-          </>
-        )}
-
-        <style>
-          {`
-            .ColorPicker_HSlider .${gamepadSliderClasses.SliderTrack} {
-              background: linear-gradient(
-                to right,
-                hsl(0, 100%, 50%),
-                hsl(60, 100%, 50%),
-                hsl(120, 100%, 50%),
-                hsl(180, 100%, 50%),
-                hsl(240, 100%, 50%),
-                hsl(300, 100%, 50%),
-                hsl(360, 100%, 50%)
-              ) !important;
-              --left-track-color: #0000 !important;
-              --colored-toggles-main-color: #0000 !important;
-            }
-          `}
-        </style>
+        <RgbSettings controller="RIGHT" />
+        <RgbSettings controller="LEFT" />
 
         <ToggleField
           label="Touchpad"
---
+++ src/components/rgb/RgbSettings.tsx
@@ -0,0 +1,116 @@
+import {
+  ToggleField,
+  SliderField,
+  ButtonItem,
+  gamepadSliderClasses
+} from 'decky-frontend-lib';
+import { FC, useState } from 'react';
+import { useRgb, useRgbMode } from '../../hooks/rgb';
+import RgbModeSlider from './RgbModeSlider';
+import { RgbModes, ControllerType } from '../../backend/constants';
+import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+
+const labelMap = {
+  RIGHT: 'Right',
+  LEFT: 'Left'
+};
+
+export const RgbSettings: FC<{ controller: ControllerType }> = ({
+  controller
+}) => {
+  const rgb = useRgb(controller);
+  const { enabled, brightness, speed, hue } = rgb.rgbInfo;
+  const [showOptions, setShowOptions] = useState(false);
+  const [mode] = useRgbMode(controller);
+
+  return (
+    <>
+      <ToggleField
+        label={`${labelMap[controller]} Controller LED`}
+        checked={enabled}
+        onChange={rgb.setEnabled}
+        bottomSeparator={enabled ? 'none' : 'thick'}
+      />
+      {enabled && (
+        <>
+          <ButtonItem
+            layout={'below'}
+            bottomSeparator={showOptions ? 'none' : 'thick'}
+            style={{
+              width: '100%',
+              height: '20px',
+              display: 'flex', // Set the display to flex
+              justifyContent: 'center', // Center align horizontally
+              alignItems: 'center' // Center align vertically
+            }}
+            onClick={() => setShowOptions(!showOptions)}
+          >
+            {showOptions ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+          </ButtonItem>
+        </>
+      )}
+      {showOptions && enabled && (
+        <>
+          <RgbModeSlider controller={controller} />
+          <SliderField
+            label={`${controller[0]}-Stick Brightness`}
+            valueSuffix="%"
+            value={brightness}
+            showValue={true}
+            min={1}
+            max={100}
+            validValues="range"
+            bottomSeparator={'none'}
+            onChange={(value) => rgb.updateBrightness(value)}
+          ></SliderField>
+          {mode !== RgbModes.SOLID && (
+            <SliderField
+              label="Speed"
+              valueSuffix="%"
+              value={speed}
+              showValue={true}
+              min={1}
+              max={100}
+              validValues="range"
+              onChange={(value) => rgb.setSpeed(value)}
+            ></SliderField>
+          )}
+          {mode !== RgbModes.DYNAMIC && (
+            <>
+              <div className="ColorPicker_HSlider">
+                <SliderField
+                  showValue
+                  label="Hue"
+                  value={hue}
+                  min={0}
+                  max={359}
+                  validValues="range"
+                  bottomSeparator="thick"
+                  onChange={(value) => rgb.setHue(value)}
+                />
+              </div>
+            </>
+          )}
+          <style>
+            {`
+            .ColorPicker_HSlider .${gamepadSliderClasses.SliderTrack} {
+              background: linear-gradient(
+                to right,
+                hsl(0, 100%, 50%),
+                hsl(60, 100%, 50%),
+                hsl(120, 100%, 50%),
+                hsl(180, 100%, 50%),
+                hsl(240, 100%, 50%),
+                hsl(300, 100%, 50%),
+                hsl(360, 100%, 50%)
+              ) !important;
+              --left-track-color: #0000 !important;
+              --colored-toggles-main-color: #0000 !important;
+            }
+          `}
+          </style>
+        </>
+      )}
+    </>
+  );
+};
