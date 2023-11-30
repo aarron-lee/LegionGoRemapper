0ca78d477f6b9f2cbecc5289c3714b7d5a24684d
Aarron Lee <aclee91@gmail.com>
2023-11-29 21:37:22 -0500

    reorganize + cleanup backed code (#14)

--- py_modules/controller_settings.py
+++ py_modules/controller_settings.py
@@ -1,8 +1,5 @@
 import os
-import decky_plugin
-import legion_configurator
 from settings import SettingsManager
-from controller_enums import Controller, RemappableButtons, RemapActions, GyroRemapActions, Gyro
 
 settings_directory = os.environ["DECKY_PLUGIN_SETTINGS_DIR"]
 settings_path = os.path.join(settings_directory, 'settings.json')
@@ -90,53 +87,3 @@ def set_all_controller_profiles(controller_profiles):
 
     setting_file.settings['controller'] = profiles
     setting_file.commit()
-
-def sync_controller_profile_settings(current_game_id):
-    settings = get_settings()
-    controller_profile = settings.get('controller').get(current_game_id, {})
-
-    try:
-        for remappable_button_name, remap_action in controller_profile.items():
-            controller_code = None
-            if remappable_button_name in ['Y3', 'M2', 'M3']:
-                controller_code = Controller['RIGHT'].value
-            elif remappable_button_name in ['Y1', 'Y2']:
-                controller_code = Controller['LEFT'].value
-            if not controller_code:
-                continue
-
-            btn_code = RemappableButtons[remappable_button_name].value
-            action_code = RemapActions[remap_action].value
-            remap_command = legion_configurator.create_button_remap_command(controller_code, btn_code, action_code)
-
-            legion_configurator.send_command(remap_command)
-    except Exception as e:
-        decky_plugin.logger.error(f"sync_controller_profile_settings: {e}")
-
-def sync_touchpad(current_game_id):
-    settings = get_settings()
-    controller_profile = settings.get('controller').get(current_game_id, {})
-
-    try:
-        new_touchpad_state = bool(controller_profile.get('TOUCHPAD'))
-        t_toggle = legion_configurator.create_touchpad_command(new_touchpad_state)
-        legion_configurator.send_command(t_toggle)
-    except Exception as e:
-        decky_plugin.logger.error(f"sync_touchpad: {e}")
-
-def sync_gyros(current_game_id):
-    settings = get_settings()
-    controller_profile = settings.get('controller').get(current_game_id, {})
-
-    for gyro in [g.name for g in Gyro]:
-        try:
-            gyro_remap_action = controller_profile.get(gyro)
-            gyro_remap_action_code = GyroRemapActions[gyro_remap_action].value
-            gyro_code = Gyro[gyro].value
-
-            if gyro_remap_action_code >= 0 and gyro_code > 0:
-                # valid codes, set gyro
-                gyro_command = legion_configurator.create_gyro_remap_command(gyro_code, gyro_remap_action_code)
-                legion_configurator.send_command(gyro_command)
-        except Exception as e:
-            decky_plugin.logger.error(f"sync_gyros {e}")
