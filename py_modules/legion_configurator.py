b3d4f4bd64660847174bf0ed7633ed2d792f26a4
Aarron Lee <aclee91@gmail.com>
2023-12-10 12:27:39 -0500

    fetch config every time command is sent

--- py_modules/legion_configurator.py
+++ py_modules/legion_configurator.py
@@ -5,24 +5,28 @@ import decky_plugin
 vendor_id = 0x17EF
 product_id_match = lambda x: x & 0xFFF0 == 0x6180
 usage_page = 0xFFA0
-global_config = None  # Global configuration for the device
+global_config = None
 
-try:
-    # Enumerate and set the global configuration
-    for dev in hid.enumerate(vendor_id):
-        if product_id_match(dev["product_id"]) and dev["usage_page"] == usage_page:
-            global_config = dev
-            break
-except Exception as e:
-    decky_plugin.logger.error("legion_configurator error: couldn't find device config {e}")
+def get_config():
+    global global_config
 
-if not global_config:
-    decky_plugin.logger.error("Legion go configuration device not found.")
-else:
-    decky_plugin.logger.info(global_config)
+    try:
+        # Enumerate and set the global configuration
+        for dev in hid.enumerate(vendor_id):
+            if product_id_match(dev["product_id"]) and dev["usage_page"] == usage_page:
+                global_config = dev
+                break
+    except Exception as e:
+        decky_plugin.logger.error("legion_configurator error: couldn't find device config {e}")
 
+    if not global_config:
+        decky_plugin.logger.error("Legion go configuration device not found.")
+    else:
+        decky_plugin.logger.info(global_config)
 
 def send_command(command):
+    global global_config
+    get_config()
     assert len(command) == 64 and global_config
     try:
         with hid.Device(path=global_config['path']) as device:
