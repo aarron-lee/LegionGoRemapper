3b185776fa923643c92c4d83290f6f9625aa689c
Aarron Lee <aarron-lee@users.noreply.github.com>
2024-04-22 20:35:02 -0400

    update reload.sh

--- reload.sh
+++ reload.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
 pnpm run build
-sudo rm -R /home/deck/homebrew/plugins/LegionGoRemapper/
-sudo cp -R /home/deck/Development/LegionGoRemapper/ /home/deck/homebrew/plugins/
+sudo rm -R $HOME/homebrew/plugins/LegionGoRemapper/
+sudo cp -R ../LegionGoRemapper/ $HOME/homebrew/plugins/
 sudo systemctl restart plugin_loader.service
